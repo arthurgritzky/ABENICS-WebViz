14253793d76bf96176550161a7d5d9c8a1e08c4c
Arthur Gritzky <89546471+arthurgritzky@users.noreply.github.com>
2025-01-30 03:48:56 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
-import { STLLoader } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/loaders/STLLoader.js';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
 
 
 // Scene setup
